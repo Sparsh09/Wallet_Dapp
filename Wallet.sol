c0d8277c0c5172d9f80594f8a6505d5be14a3800
Sparsh09 <sparsh1698.cse18@chitkara.edu.in>
2022-05-14 00:17:58 +0530

    added events , safemath library

--- Wallet.sol
+++ Wallet.sol
@@ -1,12 +1,19 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.5.0 <0.9.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 
 
 contract Allowance is Ownable{
+    
+    using SafeMath for uint;
+
+    event AllowanceChanged(address indexed _forWho, address indexed _fromWhom , uint _oldAmount , uint _NewAmount);
+
     mapping(address => uint ) public allowance;
 
     function addAllowance(address _who, uint _amount) public onlyOwner{
+        emit AllowanceChanged(_who ,msg.sender , allowance[_who] , _amount);
         allowance[_who] = _amount;
     }
 
@@ -15,26 +22,36 @@ contract Allowance is Ownable{
         _;
     }
 
-    function reduceAllowance(address _to , uint _amount) private{
-        allowance[_to] -= _amount; 
+    function reduceAllowance(address _to , uint _amount) internal{
+        emit AllowanceChanged(_to , msg.sender , allowance[_to], allowance[_to].sub(_amount));
+        allowance[_to] = allowance[_to].sub(_amount); 
+        
     }
 
 }
 
 contract Wallet is Allowance{
 
+    event SpentMoney(address indexed _person, uint _amount);
+    event MoneyReceived(uint _amount);
+
     function withdrawMoney(address payable _to , uint _amount) public ownerOrAllowed(_amount){
         require(_amount <= address(this).balance , "Amount should be less than equal to smart contact");
         if(owner() != msg.sender){
             reduceAllowance(_to , _amount);
         }
+        emit SpentMoney(_to, _amount);
         _to.transfer(_amount);
         
     }
 
-    receive() external payable {  }
+    receive() external payable { 
+        emit MoneyReceived(msg.value);
+     }
 
-    fallback () external payable{  }
+    fallback () external payable{ 
+        emit MoneyReceived(msg.value);
+     }
 
     function getContractBalance() public view returns(uint){
         return address(this).balance;
